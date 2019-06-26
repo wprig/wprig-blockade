85e1b3d3ab1a824feab4fb8ba0528093dffff5b3
Felix Arntz <felixarntz@google.com>
2019-06-26 18:25:46 +0200

    Integrate with Block Areas plugin by rendering header and footer block area.

--- header.php
+++ header.php
@@ -36,9 +36,5 @@ namespace WP_Rig\WP_Rig;
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'wp-rig' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<?php get_template_part( 'template-parts/header/custom_header' ); ?>
-
-		<?php get_template_part( 'template-parts/header/branding' ); ?>
-
-		<?php get_template_part( 'template-parts/header/navigation' ); ?>
+		<?php block_areas()->render( 'header' ); ?>
 	</header><!-- #masthead -->
